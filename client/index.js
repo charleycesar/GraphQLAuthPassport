a01c90e3d7244d8c27e92cadc3e59e319a3cdf1d
Charley Oliveira <charleycesar@gmail.com>
2021-06-13 20:39:42 -0300

    feat: create requireAuth to protect dashboard

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm'
 import Dashboard from './components/Dashboard';
+import requiredAuth from './components/requireAuth';
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -19,7 +20,7 @@ const Root = () => {
         <Route path='/' component={App}>
           <Route path='login' component={LoginForm} />
           <Route path='signup' component={SignupForm} />
-          <Route path='dashboard' component={Dashboard} />
+          <Route path='dashboard' component={requiredAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
